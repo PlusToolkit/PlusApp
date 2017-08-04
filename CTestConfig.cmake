60626170cf2e4d02f60ce02df601f89c0f35d0a3
Andras Lasso <lasso@queensu.ca>
2017-08-03 21:10:01 -0400

    re #1215: Updated dashboard URL

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,6 +8,6 @@ set(CTEST_PROJECT_NAME "PlusApp")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "crunch.cs.queensu.ca")
+set(CTEST_DROP_SITE "perkdata.cs.queensu.ca")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=PlusApp")
 set(CTEST_DROP_SITE_CDASH TRUE)
